5d38576174004708d0e09599980082acdc0ed8da
JunSeong <ljs1898@gmail.com>
2024-05-07 16:25:33 +0900

    240507 todo list, 날씨영역 추가

--- js/todo-app.js
+++ js/todo-app.js
@@ -1,7 +1,9 @@
 
-/**
- * 시계영역
- */
+/* 날씨정보 API KEY */
+const API_KEY = 'b306de69216d048912b66ddeb322d4cc';
+
+
+/* 시계영역 */
 const $clock = document.querySelector('.clock');
 
 const getClock = () => {
@@ -16,9 +18,7 @@ const seconds = setInterval(getClock, 1000);
 
 
 
-/**
- * 명연영역
- */
+/* 명연영역 */
 const quotes = [
 	{
 		quote: 'I never dreamed about success, I worked for it',
@@ -70,6 +70,92 @@ $author.innerText = toDaysQuote.author;
 
 
 
-/**
- * 배경역역
- */
+/* todo */
+const $addListInput = document.querySelector('.add-list');
+const $todoListWrap = document.querySelector('.todo-list');
+
+const TODO_LIST_KEY = 'todoList';
+
+let todoListArray = [];
+
+const saveTodoList = () => {
+	localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoListArray));
+}
+
+const deleteTodoList = (e) => {
+	const $targetLi = e.target.parentElement;
+	$targetLi.remove();
+	todoListArray = todoListArray.filter( todo => todo.id !== parseInt($targetLi.id) );
+	saveTodoList();
+}
+
+const paintTodo = (newTodo) => {
+	const todoListLi = document.createElement('li');
+	todoListLi.id = newTodo.id;
+	const todoListText = document.createElement('span');
+	const removeButton = document.createElement('button');
+
+	removeButton.setAttribute('type', 'button');
+	removeButton.innerText = '❌';
+
+	todoListLi.appendChild(todoListText);
+	todoListText.innerText = newTodo.text;
+	todoListLi.appendChild(removeButton);
+
+	$todoListWrap.appendChild(todoListLi);
+
+	removeButton.addEventListener('click', deleteTodoList)
+}
+
+const addListFn = (e) => {
+	if(e.keyCode === 13) {
+		const newTodo = $addListInput.value;
+		$addListInput.value = "";
+		const newTodoObj = {
+			text: newTodo,
+			id: Date.now(),
+		}
+		todoListArray.push(newTodoObj);
+		paintTodo(newTodoObj);
+		saveTodoList();
+	}
+}
+
+$addListInput.addEventListener('keydown', (e) => {
+	addListFn(e);
+})
+
+const savedTodoList = localStorage.getItem(TODO_LIST_KEY);
+
+if (savedTodoList) {
+	const parsedTodoList = JSON.parse(savedTodoList);
+	todoListArray = parsedTodoList
+	parsedTodoList.forEach(paintTodo);
+}
+
+
+
+/* 날씨영역 */
+const onGeoSuccess = (position) => {
+	const lat = position.coords.latitude;
+	const lng = position.coords.longitude;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`;
+	const $city = document.querySelector('.city');
+	const $weathr = document.querySelector('.city-weather');
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			$city.innerText = `지역 : ${data.name}`;
+			$weathr.innerText = `날씨 : ${data.weather[0].main} / 온도 : ${data.main.temp}`;
+		})
+		.catch(error => {
+			console.error('Error fetching URL data:', error);
+		})
+}
+const onGeoError = () => {
+	alert("위치를 찾지 못하였습니다. 날씨정보를 얻지 못하였습니다.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
